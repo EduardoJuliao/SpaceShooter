9ecbee717acf6a00311f45bc2b853bd52d13aa43
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-06-30 22:30:41 +0100

    Reuse of an enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * (_speed * Time.deltaTime));
+
+        if (transform.position.y < Boundries.MinY - 2f)
+        {
+            var newX = Random.Range(Boundries.MinX, Boundries.MaxX);
+            transform.position = new Vector3(newX, Boundries.MaxY + 2, 0);
+        }
+    }
+}
