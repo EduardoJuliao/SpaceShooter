2b8bfaaf02f5c2239f6ad916eb92ff6f6c0e376d
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-06-30 23:06:55 +0100

    Trigger collision

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.UIElements;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _speed = 4.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -21,6 +23,19 @@ public class Enemy : MonoBehaviour
         {
             var newX = Random.Range(Boundries.MinX, Boundries.MaxX);
             transform.position = new Vector3(newX, Boundries.MaxY + 2, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Laser"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
         }
     }
 }
