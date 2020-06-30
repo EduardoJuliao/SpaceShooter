2532704ffcb63b81facd81f47d48cc4cb7e17135
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-06-30 23:28:15 +0100

    Spawn Enemies

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private float _secondsToSpawn;
+
+    private void Start()
     {
-        
+        StartCoroutine(SpawnEnemies());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnEnemies()
     {
-        
+        while (true)
+        {
+            var newX = Random.Range(Boundries.MinX, Boundries.MaxX);
+            Instantiate(_enemyPrefab, new Vector3(newX, Boundries.MaxY + 2f, 0), Quaternion.identity);
+            
+            yield return new WaitForSeconds(_secondsToSpawn);
+        }
     }
 }
