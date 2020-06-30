5c890810a1bc87f6d6c8ff794f604f37cffe1f77
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-06-30 21:29:37 +0100

    player speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,18 +2,19 @@
 
 public class Player : MonoBehaviour
 {
-
+    [SerializeField] private float _speed = 3.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-
         transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Time.deltaTime converts to real human time
+        // Equivalent of incorporating real time
+        transform.Translate(Vector3.right * (_speed * Time.deltaTime));
     }
 }
