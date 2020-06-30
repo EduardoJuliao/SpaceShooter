1425cb87844e4e7eeb740d5613d4d646e2695207
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-06-30 21:50:43 +0100

    Code cleanup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,14 +14,38 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         // Time.deltaTime converts to real human time
         // Equivalent of incorporating real time
 
         var horizontalInput = Input.GetAxis("Horizontal");
         var verticalInput = Input.GetAxis("Vertical");
+        var playerTransform = transform;
 
         var direction = new Vector3(horizontalInput, verticalInput, 0);
 
-        transform.Translate(direction * (_speed * Time.deltaTime));
+        playerTransform.Translate(direction * (_speed * Time.deltaTime));
+
+        if (transform.position.x >= 9)
+        {
+            playerTransform.position = new Vector3(9, transform.position.y, 0);
+        } 
+        else if (transform.position.x <= -9)
+        {
+            playerTransform.position = new Vector3(-9, transform.position.y, 0);
+        }
+        
+        if (transform.position.y <= -3.8)
+        {
+            playerTransform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+        else if (transform.position.y >= 5.5)
+        {
+            playerTransform.position = new Vector3(transform.position.x, 5.5f, 0);
+        }
     }
 }
