cbc80a2e63ca4d79f8f23f9a0b5b25113858cca9
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-06-30 21:15:12 +0100

    Initial Player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
