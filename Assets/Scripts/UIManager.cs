19e229f0b3124f2286912e26e885b2d67db0f978
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-07-07 02:17:16 +0100

    Game over text

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,12 +7,14 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private Text _scoreText;
+    [SerializeField] private Text _gameOverText;
     [SerializeField] private Image _livesDisplay;
     [SerializeField] private Sprite[] _liveSprites;
 
     private void Start()
     {
         UpdateScoreText(0);
+        _gameOverText.gameObject.SetActive(false);
     }
 
     public void UpdateScoreText(long score)
@@ -23,5 +25,9 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int lives)
     {
         _livesDisplay.sprite = _liveSprites[lives];
+        if (lives <= 0)
+        {
+            _gameOverText.gameObject.SetActive(true);
+        }
     }
 }
