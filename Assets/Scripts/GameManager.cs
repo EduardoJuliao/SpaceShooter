6bfdeab1d88e58e7891dd401aed5eee818654d76
Eduardo julião <eduardo.juliaojr@hotmail.com>
2020-07-07 02:35:40 +0100

    Restart level

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool _isGameOver = false;
+
+    public void GameOver()
+    {
+        _isGameOver = true;
+    }
+
+    private void Update()
+    {
+        if (_isGameOver && Input.GetKeyUp(KeyCode.R))
+        {
+            SceneManager.LoadScene("Scenes/Game");
+        }
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,19 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private Text _scoreText;
     [SerializeField] private Text _gameOverText;
+    [SerializeField] private Text _restartText;
     [SerializeField] private Image _livesDisplay;
     [SerializeField] private Sprite[] _liveSprites;
+
+    private GameManager _gameManager;
 
     private void Start()
     {
         UpdateScoreText(0);
         _gameOverText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        if (_gameManager == null)
+        {
+            Debug.LogError("Game Manager is null.");
+        }
     }
 
     public void UpdateScoreText(long score)
@@ -27,9 +37,16 @@ public class UIManager : MonoBehaviour
         _livesDisplay.sprite = _liveSprites[lives];
         if (lives <= 0)
         {
-            _gameOverText.gameObject.SetActive(true);
-            StartCoroutine(GameOverFlicker());
+            GameOver();
         }
+    }
+
+    private void GameOver()
+    {
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);
+        _gameManager.GameOver();
+        StartCoroutine(GameOverFlicker());
     }
 
     IEnumerator GameOverFlicker()
